2be04a3d76d23702641bc00eec5a3dea8f189346
theallisonbrett <theallisonbrett@gmail.com>
2017-06-07 19:39:25 -0700

    Add render function

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ var state = {
 function addItems(state, item) {
   state.items.push(item);
 }
+
+var renderList = function (state,element) {
+  var itemsHTML = state.items.map(function(item) {
+    return item;
+  });
+  elements.html(itemsHTML);
+};
