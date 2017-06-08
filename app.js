3a3c9d9cb41d17aa460de660cb8a29c963fd7066
theallisonbrett <theallisonbrett@gmail.com>
2017-06-08 13:19:26 -0700

    Add class template and set to display none

--- app.js
+++ app.js
@@ -6,23 +6,23 @@ function addItems(state, item){
   state.items.push(item);
 }
 
-function renderHTML(state) {
-$(".shopping-item").append(state.items);
-}
+// function renderHTML(state) {
+// $(".shopping-item").append(state.items);
+// }
 
-// var renderList = function (state,element) {
-//   var itemsHTML = state.items.map(function(item) {
-//     return item;
-//   });
-//   elements.html(itemsHTML);
-// };
+var buildList = function (state,element) {
+  var itemsHTML = state.items.map(function(item) {
+    return item;
+  });
+  element.html(itemsHTML);
+};
 
 function formSubmit() {
 $('#js-shopping-list-form').submit(function(event) {
   event.preventDefault();
   addItems(state, $('#shopping-list-entry').val());
-  // renderList(state, $('.shopping-item'));
-  renderHTML(state);
+  buildList(state, $('.shopping-item'));
+  // renderHTML(state);
 
 });
 }
