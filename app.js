df3478215f4681e819ce850975eaf9226887c341
theallisonbrett <theallisonbrett@gmail.com>
2017-06-07 20:02:20 -0700

    Change class to ID

--- app.js
+++ app.js
@@ -2,22 +2,23 @@ var state = {
   items: []
 };
 
-function addItems(state, item) {
+function addItems(state, item){
   state.items.push(item);
 }
 
-var renderList = function (state,element) {
-  var itemsHTML = state.items.map(function(item) {
-    return item;
-  });
-  elements.html(itemsHTML);
-};
+// var renderList = function (state,element) {
+//   var itemsHTML = state.items.map(function(item) {
+//     return item;
+//   });
+//   elements.html(itemsHTML);
+// };
 
 function formSubmit() {
-$('.js-shopping-list-form').submit(function(event) {
+$('#js-shopping-list-form').submit(function(event) {
   event.preventDefault();
   addItems(state, $('#shopping-list-entry').val());
   renderList(state, $('.shopping-item'));
+
 });
 }
 
