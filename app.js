0b387a5650f474ca169d2d6033634b72bbcb72d1
theallisonbrett <theallisonbrett@gmail.com>
2017-06-07 20:09:15 -0700

    Add function render HTML

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ function addItems(state, item){
   state.items.push(item);
 }
 
+function renderHTML(state) {
+$(".shopping-item").append(state.items);
+}
+
 // var renderList = function (state,element) {
 //   var itemsHTML = state.items.map(function(item) {
 //     return item;
@@ -17,7 +21,8 @@ function formSubmit() {
 $('#js-shopping-list-form').submit(function(event) {
   event.preventDefault();
   addItems(state, $('#shopping-list-entry').val());
-  renderList(state, $('.shopping-item'));
+  // renderList(state, $('.shopping-item'));
+  renderHTML(state);
 
 });
 }
